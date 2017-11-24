7175a971876aad84b03ce6d71fc706650c54d393
Dayne Beck <daynebeck28@gmail.com>
2017-11-23 19:41:06 -0600

    Add smooth scrolling

--- dbeck28.github.io/js/ffg.js
+++ dbeck28.github.io/js/ffg.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+    $(".menu-container .menu .about a").on('click', function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $(".welcome-container").offset().top
+          }, 800);
+    })// End if
+
+    $(".menu-container .menu .services a").on('click', function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $(".our-services-container").offset().top
+          }, 800);
+    })
+
+    $(".menu-container .menu .contact a").on('click', function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $(".contact-container").offset().top
+          }, 800);
+    })
+
+    $(".menu-container .menu .our-team a").on('click', function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $(".team-header").offset().top
+          }, 800);
+    })
+
   $('.our-services-container .services .service-list div#armed-content').hide()
   $('.our-services-container .services .service-list div#business-profile-content').hide()
   $('.our-services-container .services .service-list div#child-support-content').hide()
